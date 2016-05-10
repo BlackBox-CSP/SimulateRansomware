6bf02efde49f415e0a62bdc4a412840032ac1df0
unknown <rbrockw@RBROCKWe6420.corp.bbns.com>
2016-05-10 10:54:07 -0400

    initial version

---
+++ SimulateRansomware3.ps1
@@ -0,0 +1,13 @@
+﻿$TARGETPATH = [Environment]::GetFolderPath("MyDocuments")
+$TARGETDIR = "$TARGETPATH\EncryptionTest\"
+if(!(Test-Path -Path $TARGETDIR)){
+    New-Item -ItemType directory -Path $TARGETDIR
+}
+GCI $TARGETDIR | Remove-Item -Force
+1..2 | % { 
+    $TARGETFILE = $TARGETDIR + $_ + ".txt";
+    Out-File $TARGETFILE
+    for ($i=1; $i -le 4096; $i++) {
+        -join ((33..122) | Get-Random -Count 1 | % {[char]$_}) | Add-Content $TARGETFILE 
+        }
+    }
